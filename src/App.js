b22be70806cff1c2e1288f4815d620abb652c31b
Daniel Zhang <zhangqiangtianchen@gmail.com>
2022-11-08 17:34:25 -0800

    modified css, added link to bootstrap

--- src/App.js
+++ src/App.js
@@ -1,43 +1,27 @@
-import React, {useState,useEffect} from 'react';
-
-import './App.css';
-import Header from './components/Header/Header';
-import Hero from './components/UI/Hero';
-// import Counter from './components/UI/Counter';
-import Services from './components/UI/Services';
-// import About from './components/UI/About';
-// import Team from './components/UI/Team';
-// import Blog from './components/UI/Blog';
-import Review from './components/UI/Review';
-import Search from './components/UI/Newsletter';
-import Footer from './components/Footer/Footer';
+import { BrowserRouter, Router, Routes, Route, Switch } from "react-router-dom";
+import './App.css'
+import Home from './components/UI/Home'
+import Profile from "./components/UI/Profile";
 
 function App() {
-
-  const [theme, setTheme] = useState ('')
-
-  const toggleTheme = ()=> {
-    theme === '' ? setTheme ('light-theme') : setTheme ('')
-  }
-
-  useEffect(()=>{
-    document.body.className = theme
-  },[theme])
-
-  return (
-    <>
-    <Header theme={theme} toggleTheme={toggleTheme} />
-    {false && <Hero theme={theme} />}
-    {/* <Counter /> */}
-    <Search />
-    <Services />
-    {/* <About /> */}
-    {/* <Team /> */}
-    {/* <Blog /> */}
-    <Review />
-    <Footer />
-    </>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+            
+                <Routes>
+                    <Route
+                        exact path="/"
+                        element={<Home />}
+                    />
+                    <Route
+                        path="/profile"
+                        element={<Profile />}
+                    />
+                </Routes>
+            
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
