83c398911207bc4f4f6a73dc7c648e107cd18bbb
JazC <jzchan7713@gmail.com>
2022-11-10 09:01:29 -0800

    Separate login and signup modals, adjusted css

--- src/components/Header/LoginModal.jsx
+++ src/components/Header/LoginModal.jsx
@@ -2,17 +2,14 @@ import { React, useState } from "react";
 import "./LoginModal.css";
 import { Form, Button } from "react-bootstrap";
 
-
-
 function LoginModal({ setOpenModal }) {
 
-  const [username, setUserName] = useState(null);
   const [password, setPassword] = useState(null);
   const [email, setEmail] = useState(null);
 
   return (
     <div className="modalBackground">
-      <div className="modalContainer" style={{ height: '50%' }}>
+      <div className="modalContainer" style={{ height: '65%' }}>
         <div className="titleCloseBtn">
           <button
             onClick={() => {
@@ -27,19 +24,15 @@ function LoginModal({ setOpenModal }) {
           <p style={{ textAlign: 'center' }}>Not a member yet? <a style={{ color: 'black', textDecoration: 'underline' }}>Sign up </a></p>
         </div>
         <Form style={{ margin: 'auto', marginTop: '50px', textAlign: 'center', width: '100%' }}>
-          <div class="mb-3">
-            <label className="form-label">User Name</label>
-            <input type="username" class="form-control" required onChange={(e) => setUserName(e.target.value)} placeholder="username" />
-          </div>
 
           <div class="mb-3">
-            <label className="form-label">Email</label>
+            <p className="form-label" style={{ textAlign: 'left' }}>Email</p>
             <input type="email" class="form-control" required onChange={(e) => setEmail(e.target.value)} placeholder="name@example.com" />
           </div>
 
           <div class="mb-3">
-            <label className="form-label">Password</label>
-            <input type="password" class="form-control" required onChange={(e) => setPassword(e.target.value)} />
+          <p className="form-label" style={{ textAlign: 'left' }}>Password</p>
+            <input type="password" class="form-control" required onChange={(e) => setPassword(e.target.value)} placeholder="password" />
           </div>
           <Button variant="primary" type="submit" style={{ height: '40px', width: '80%', borderRadius: '6px', background: '#0c123d', color: 'white', fontWeight: 'bold' }}>
             Submit
